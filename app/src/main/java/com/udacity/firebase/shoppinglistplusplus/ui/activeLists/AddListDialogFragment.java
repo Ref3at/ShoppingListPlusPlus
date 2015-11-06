435158f9a07f1203cc43e8cdb6824e6a04f4d70b
Lyla <lyla@udacity.com>
2015-11-06 15:23:42 -0800

    4.08 User Friends - Solution

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/AddListDialogFragment.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/AddListDialogFragment.java
@@ -121,7 +121,7 @@ public class AddListDialogFragment extends DialogFragment {
 
             /* HashMap for data to update */
             HashMap<String, Object> updateShoppingListData = new HashMap<>();
-            
+
             /**
              * Set raw version of date to the ServerValue.TIMESTAMP value and save into
              * timestampCreatedMap
